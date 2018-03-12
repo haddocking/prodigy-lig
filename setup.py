3b1f49b36a0d50f29137d0db5534912925cc50d0
pkoukos <pkoukos2009@hotmail.com>
2018-03-12 16:12:53 +0100

    Update the setup script

--- setup.py
+++ setup.py
@@ -1,11 +1,33 @@
+import os
 from setuptools import setup
 
-setup(name='prodigy_lig',
-      version='0.0.1',
-      description='Calculate the Binding Affinity score using the PRODIGY-LIG model',
-      url='http://github.com/haddocking/prodigy-lig',
-      author='Computational Structural Biology Group @ Utrecht University',
-      author_email='',
-      license='Apache 2.0',
-      packages=['prodigy_lig'],
-      zip_safe=False)
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+requirements = [
+    "numpy",
+    "biopython",
+]
+
+setup(
+    name='prodigy_lig',
+    version = "0.0.0",
+    description=("Calculate protein-small molecule binding affinities."),
+    url='http://github.com/haddocking/prodigy-lig',
+    author='Computational Structural Biology Group @ Utrecht University',
+    author_email='prodigy.bonvilab@gmail.com',
+    license='Apache 2.0',
+    packages=['prodigy_lig'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+    ],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'prodigy_lig = prodigy_lig.prodigy_lig:main',
+        ]
+    },
+    zip_safe=False
+)
